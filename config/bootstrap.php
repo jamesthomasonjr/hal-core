4494f8adb0086b34b47ae3cefe376aa49d16e3ac
Steve Kluck <stevekluck@quickenloans.com>
2014-10-14 08:52:05 -0400

    Update bins, add doctrine bin

---
+++ config/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Bootstrap;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+$root = __DIR__ . '/../';
+require_once $root . '/vendor/autoload.php';
+
+// Set Timezone to UTC
+ini_set('date.timezone', 'UTC');
+date_default_timezone_set('UTC');
+
+$container = new ContainerBuilder;
+$builder = new YamlFileLoader($container, new FileLocator($root));
+$builder->load('config/config.example.yml');
+$builder->load('config/di.yml');
+
+$container->compile();
+
+return $container;
