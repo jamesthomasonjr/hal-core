06a492eb97a14fa94ac6ad855554b610f95e56bb
Steve Kluck <stevekluck@quickenloans.com>
2014-10-14 09:06:54 -0400

    Update some docblocks

--- src/Entity/Environment.php
+++ src/Entity/Environment.php
@@ -1,5 +1,9 @@
 <?php
-# src/QL/Hal/Core/Entity/Environment.php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
 
 namespace QL\Hal\Core\Entity;
 
