ef753b7992219baec5bb8b49ecee452279756626
Steve Kluck <stevekluck@quickenloans.com>
2014-12-30 09:16:46 -0500

    Move "get latest push" queries from deployment repo to push repo

--- src/Entity/Repository/PushRepository.php
+++ src/Entity/Repository/PushRepository.php
@@ -8,11 +8,50 @@
 namespace QL\Hal\Core\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use QL\Hal\Core\Entity\Server;
+use QL\Hal\Core\Entity\Deployment;
+use QL\Hal\Core\Entity\Push;
 use QL\Hal\Core\Entity\Repository;
+use QL\Hal\Core\Entity\Server;
 
 class PushRepository extends EntityRepository
 {
+    const DQL_ROLLBACKS = <<<SQL
+SELECT p
+  FROM QL\Hal\Core\Entity\Push p
+  JOIN p.deployment d
+  JOIN p.build b
+ WHERE
+    d.server = :server AND
+    d.repository = :repo AND
+    p.status = :pushStatus AND
+    b.status = :buildStatus
+ ORDER BY p.end DESC
+SQL;
+
+    const DQL_BY_REPOSITORY = <<<SQL
+SELECT p
+  FROM QL\Hal\Core\Entity\Push p
+  JOIN p.deployment d
+ WHERE
+    d.repository = :repo
+ ORDER BY p.end DESC
+SQL;
+
+    const DQL_RECENT_PUSH = <<<SQL
+SELECT p
+FROM QL\Hal\Core\Entity\Push p
+WHERE p.deployment = :deploy
+ORDER BY p.created DESC
+SQL;
+    const DQL_RECENT_SUCCESSFUL_PUSH = <<<SQL
+SELECT p
+  FROM QL\Hal\Core\Entity\Push p
+ WHERE
+    p.deployment = :deploy AND
+    p.status = :status
+ ORDER BY p.created DESC
+SQL;
+
     /**
      * Get all pushes, with available builds, that can be used as a rollback
      *
@@ -23,16 +62,14 @@ class PushRepository extends EntityRepository
      */
     public function getAvailableRollbacks(Repository $repository, Server $server, $limit = 25)
     {
-        $dql = 'SELECT p FROM QL\Hal\Core\Entity\Push p
-            JOIN p.deployment d JOIN p.build b
-            WHERE d.server = :server AND d.repository = :repo AND p.status = :status AND b.status = :buildstatus
-            ORDER BY p.end DESC';
-        $query = $this->_em->createQuery($dql)
-                          ->setMaxResults($limit)
-                          ->setParameter('repo', $repository)
-                          ->setParameter('server', $server)
-                          ->setParameter('status', 'Success')
-                          ->setParameter('buildstatus', 'Success');
+        $query = $this->_em
+            ->createQuery(self::DQL_ROLLBACKS)
+            ->setMaxResults($limit)
+            ->setParameter('repo', $repository)
+            ->setParameter('server', $server)
+            ->setParameter('pushStatus', 'Success')
+            ->setParameter('buildStatus', 'Success');
+
         return $query->getResult();
     }
 
@@ -44,9 +81,45 @@ class PushRepository extends EntityRepository
      */
     public function getForRepository(Repository $repository)
     {
-        $dql = 'SELECT p FROM QL\Hal\Core\Entity\Push p JOIN p.deployment d WHERE d.repository = :repo ORDER BY p.end DESC';
-        $query = $this->_em->createQuery($dql)
+        $query = $this->_em
+            ->createQuery(self::DQL_BY_REPOSITORY)
             ->setParameter('repo', $repository);
+
         return $query->getResult();
+    }
+
+    /**
+     * Get the most recent push for a deployment
+     *
+     * @param Deployment $deployment
+     *
+     * @return Push|null
+     */
+    public function getMostRecentByDeployment(Deployment $deployment)
+    {
+        $query = $this->em
+            ->createQuery(self::DQL_RECENT_PUSH)
+            ->setMaxResults(1)
+            ->setParameter('deploy', $deployment);
+
+        return $query->getOneOrNullResult();
+    }
+
+    /**
+     * Get the most recent successful push for a deployment
+     *
+     * @param Deployment $deployment
+     *
+     * @return Push|null
+     */
+    public function getMostRecentSuccessByDeployment(Deployment $deployment)
+    {
+        $query = $this->em
+            ->createQuery(self::DQL_RECENT_SUCCESSFUL_PUSH)
+            ->setMaxResults(1)
+            ->setParameter('deploy', $deployment)
+            ->setParameter('status', 'Success');
+
+        return $query->getOneOrNullResult();
     }
 }
