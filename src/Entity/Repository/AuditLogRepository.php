0b3de335fca6acd957b7167a7088332279f0f36d
Steve Kluck <stevekluck@quickenloans.com>
2015-01-05 15:04:33 -0500

    Add paged result queries for builds and audit logs

--- src/Entity/Repository/AuditLogRepository.php
+++ src/Entity/Repository/AuditLogRepository.php
@@ -8,7 +8,31 @@
 namespace QL\Hal\Core\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class AuditLogRepository extends EntityRepository
 {
+    const DQL_GET_PAGED = <<<SQL
+  SELECT l
+    FROM QL\Hal\Core\Entity\AuditLog l
+ORDER BY l.recorded DESC
+SQL;
+
+    /**
+     * Get all audit logs, paged
+     *
+     * @param int $limit
+     * @param int $page
+     *
+     * @return Paginator
+     */
+    public function getPagedResults($limit = 25, $page = 0)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery(self::DQL_GET_PAGED)
+            ->setMaxResults($limit)
+            ->setFirstResult($limit * $page);
+
+        return new Paginator($query);
+    }
 }
--- src/Entity/Repository/BuildRepository.php
+++ src/Entity/Repository/BuildRepository.php
@@ -8,7 +8,35 @@
 namespace QL\Hal\Core\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use QL\Hal\Core\Entity\Repository;
 
 class BuildRepository extends EntityRepository
 {
+    const DQL_BY_REPOSITORY = <<<SQL
+   SELECT b
+     FROM QL\Hal\Core\Entity\Build b
+    WHERE b.repository = :repo
+ ORDER BY b.created DESC
+SQL;
+
+    /**
+     * Get all builds for a repository
+     *
+     * @param Repository $repository
+     * @param int $limit
+     * @param int $page
+     *
+     * @return Paginator
+     */
+    public function getForRepository(Repository $repository, $limit = 25, $page = 0)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery(self::DQL_BY_REPOSITORY)
+            ->setMaxResults($limit)
+            ->setFirstResult($limit * $page)
+            ->setParameter('repo', $repository);
+
+        return new Paginator($query);
+    }
 }
