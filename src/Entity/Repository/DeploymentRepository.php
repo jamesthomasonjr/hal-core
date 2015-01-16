84d8a2620f2c7583b032a881ad735617561af32d
Steve Kluck <stevekluck@quickenloans.com>
2015-01-16 13:42:05 -0500

    Add query to get deployments for a repo,env pair

--- src/Entity/Repository/DeploymentRepository.php
+++ src/Entity/Repository/DeploymentRepository.php
@@ -8,7 +8,38 @@
 namespace QL\Hal\Core\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use QL\Hal\Core\Entity\Deployment;
+use QL\Hal\Core\Entity\Environment;
+use QL\Hal\Core\Entity\Repository;
 
 class DeploymentRepository extends EntityRepository
 {
+    const DQL_BY_REPOSITORY_AND_ENVIRONMENT = <<<SQL
+   SELECT d
+     FROM QL\Hal\Core\Entity\Deployment d
+
+     JOIN QL\Hal\Core\Entity\Server s WITH s = d.server
+     JOIN QL\Hal\Core\Entity\Environment e WITH e = s.environment
+
+    WHERE d.repository = :repo
+      AND s.environment = :env
+SQL;
+
+    /**
+     * Get all buildable environments for a repository
+     *
+     * @param Repository $repository
+     * @param Environment $environment
+     *
+     * @return Deployment[]
+     */
+    public function getDeploymentsForRepositoryEnvironment(Repository $repository, Environment $environment)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery(self::DQL_BY_REPOSITORY_AND_ENVIRONMENT)
+            ->setParameter('repo', $repository)
+            ->setParameter('env', $environment);
+
+        return $query->getResult();
+    }
 }
