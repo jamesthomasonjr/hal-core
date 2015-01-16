f496bc8834f29a620cb4b415b081c3e085f35be9
Steve Kluck <stevekluck@quickenloans.com>
2015-01-16 10:36:26 -0500

    Add query to env repo to get available envs for a repo

--- src/Entity/Repository/EnvironmentRepository.php
+++ src/Entity/Repository/EnvironmentRepository.php
@@ -8,7 +8,35 @@
 namespace QL\Hal\Core\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use QL\Hal\Core\Entity\Environment;
+use QL\Hal\Core\Entity\Repository;
 
 class EnvironmentRepository extends EntityRepository
 {
+    const DQL_BY_REPOSITORY = <<<SQL
+   SELECT e
+     FROM QL\Hal\Core\Entity\Deployment d
+
+     JOIN QL\Hal\Core\Entity\Server s WITH s = d.server
+     JOIN QL\Hal\Core\Entity\Environment e WITH e = s.environment
+
+    WHERE d.repository = :repo
+ ORDER BY e.order ASC
+SQL;
+
+    /**
+     * Get all buildable environments for a repository
+     *
+     * @param Repository $repository
+     *
+     * @return Environment[]
+     */
+    public function getBuildableEnvironmentsForRepository(Repository $repository)
+    {
+        $query = $this->getEntityManager()
+            ->createQuery(self::DQL_BY_REPOSITORY)
+            ->setParameter('repo', $repository);
+
+        return $query->getResult();
+    }
 }
