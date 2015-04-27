d5084d8a13702d560e4ddb2ab29faf5252958610
Steve Kluck <stevekluck@quickenloans.com>
2015-04-27 14:25:08 -0400

    Add convenience queries for build and push count

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -11,4 +11,53 @@ use Doctrine\ORM\EntityRepository;
 
 class UserRepository extends EntityRepository
 {
+    const DQL_BUILD_COUNT = <<<SQL
+   SELECT count(p)
+     FROM QL\Hal\Core\Entity\Build b
+    WHERE
+        b.user = :user
+SQL;
+
+    const DQL_PUSH_COUNT = <<<SQL
+   SELECT count(p)
+     FROM QL\Hal\Core\Entity\Push p
+    WHERE
+        p.user = :user
+SQL;
+
+    /**
+     * Get all number of builds for User
+     *
+     * @param User $user
+     *
+     * @return int
+     */
+    public function getBuildCount(User $user)
+    {
+        $dql = self::DQL_BUILD_COUNT;
+
+        $query = $this->getEntityManager()
+            ->createQuery($dql)
+            ->setParameter('user', $user);
+
+        return $query->getOneOrNullResult();
+    }
+
+    /**
+     * Get all number of pushes for User
+     *
+     * @param User $user
+     *
+     * @return int
+     */
+    public function getPushCount(User $user)
+    {
+        $dql = self::DQL_PUSH_COUNT;
+
+        $query = $this->getEntityManager()
+            ->createQuery($dql)
+            ->setParameter('user', $user);
+
+        return $query->getOneOrNullResult();
+    }
 }
