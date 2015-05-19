0203ae3316581f54ece7a2ade50c5bfea4ec22bb
Steve Kluck <stevekluck@quickenloans.com>
2015-05-19 08:22:45 -0400

    Add migrations for removing consumer and fixing token id to string

---
+++ src/DatabaseMeta.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @copyright ©2015 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace QL\Hal\Core;
+
+class DatabaseMeta
+{
+    const DB_LOG_AUDIT = 'AuditLogs';
+    const DB_LOG_EVENT = 'EventLogs';
+
+    const DB_GROUP = 'Groups';
+    const DB_REPO = 'Repositories';
+    const DB_ENCRYPTED = 'EncryptedProperties';
+
+    const DB_ENVIRONMENT = 'Environments';
+    const DB_SERVER = 'Servers';
+    const DB_DEPLOYMENT = 'Deployments';
+
+    const DB_BUILD = 'Builds';
+    const DB_PUSH = 'Pushes';
+
+    const DB_USER = 'Users';
+    const DB_TOKEN = 'Tokens';
+}
