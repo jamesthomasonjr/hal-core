b0375559cfb82989bdfcebc55b07c2d4102649d2
Steve Kluck <stevekluck@quickenloans.com>
2015-05-28 19:53:50 -0400

    Fix file name

--- src/Utility/SortingTrait.php
+++ src/Utility/SortingTrait.php
@@ -27,8 +27,8 @@ trait SortingTrait
 
         return function(Environment $a, Environment $b) use ($order) {
 
-            $aName = strtolower($a->name());
-            $bName = strtolower($b->name());
+            $aName = strtolower($a->getKey());
+            $bName = strtolower($b->getKey());
 
             $aOrder = isset($order[$aName]) ? $order[$aName] : 999;
             $bOrder = isset($order[$bName]) ? $order[$bName] : 999;
